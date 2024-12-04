1a2b793f915c9dc1f76f2e3c4b32cb836039455e
Wichit Sombat <wichit.s@ubu.ac.th>
2024-12-04 18:27:05 +0700

    late

--- Kouponapp/management/commands/loaddata_from_excel.py
+++ Kouponapp/management/commands/loaddata_from_excel.py
@@ -1,19 +1,42 @@
 from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
 from openpyxl import load_workbook
 from Kouponapp.models import Promotion
+import os
+from django.conf import settings
+from Kouponapp.models import *
 
 class Command(BaseCommand):
     help = "Load promotion data from data4.xlsx file"
 
     def handle(self, *args, **kwargs):
         # Path ของไฟล์ Excel
-        file_path = '/Users/yaneekumsudsang/Koupon/data4.xlsx'
+        #file_path = '/Users/yaneekumsudsang/Koupon/data4.xlsx'
+        file_path = os.path.join(settings.BASE_DIR, 'Kouponapp/fixtures/data4-2.xlsx')
 
         # Load Excel workbook
         wb = load_workbook(filename=file_path)
         ws = wb.active  # สมมติว่า sheet แรกคือที่เราต้องการอ่าน
 
+        ws = wb['Member']
+        for row in ws:
+            values = [cell.value for cell in row]
+            if values[0] != 'id':
+                #id = values[0]
+                #username = values[1]
+                user = User.objects.create_user(values[1], values[5], str(values[6]), pk=values[0], first_name=values[2], last_name=values[3])
+                member, created = Member.objects.get_or_create(user=user, phone=values[4])
+
+        ws = wb['Store']
+        # ดูที่  KhootClone week06 'load_xlsx.py'
+
+        # {
+        #   'id': 1,
+        #   'username': 'a',
+        #   'first_name': 'Abby',
+        # }
         # เริ่มอ่านข้อมูลจากแถวที่ 2
+        '''
         for row in ws.iter_rows(min_row=2, values_only=True):
             if len(row) >= 6:
                 store_name, collection_number, cup_size, discount, coupon_name, expiration_date = row[:6]
@@ -34,3 +57,4 @@ class Command(BaseCommand):
                 promotion.save()
 
         self.stdout.write(self.style.SUCCESS('Successfully loaded data from Excel'))
+        '''
--- Kouponapp/models.py
+++ Kouponapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Promotion(models.Model):
+    # id
     store_name = models.CharField(max_length=255)
     collection_number = models.CharField(max_length=50)
     cup_size = models.CharField(max_length=50)
@@ -10,3 +12,18 @@ class Promotion(models.Model):
 
     def __str__(self):
         return self.store_name
+
+class Store(models.Model):
+    # id
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=10, blank=True, null=True)
+    profile_img = models.ImageField(upload_to='profiles/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
